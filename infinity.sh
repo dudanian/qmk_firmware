f198130a85bd5de5557cdbc4cb4b7441a9438b92
Andrew Duda <dudanian@gmail.com>
2020-05-12 21:25:37 +0900

    infinity.sh: rewrite to use docker
    Rewrote the script to use docker to build the image. Building works
    completely fine.
    
    The problem is I can't run 'make dfs-util' commands without having
    dependencies installed. Got around this for now by just copying bits of
    the build system I needed. Hopefully we can actually properly decouple
    dfs-util from the greater image build system in the future.

--- infinity.sh
+++ infinity.sh
@@ -1,7 +1,47 @@
 #!/bin/sh
-# sudo early to cache the auth so I can unplug
-sudo echo "building ergodox_infinity:dudanian" && \
-  make ergodox_infinity:dudanian && \
-  sudo make ergodox_infinity:dudanian:dfu-util && \
-  make ergodox_infinity:dudanian MASTER=right && \
-  sudo make ergodox_infinity:dudanian:dfu-util MASTER=right
+
+# config for qmk
+KEYBOARD=ergodox_infinity
+KEYMAP=dudanian
+DFU_ARGS="-d 1c11:b007"
+
+# config for docker
+# XXX very much assumes this is working dir
+QMK_SRC=$(pwd)
+WORKDIR=/qmk_firmware
+USER="$(id -u):$(id -g)"
+
+# stolen from tmk_core/chibios.mk
+ERROR_COLOR=$'\033[31;01m'
+NO_COLOR=$'\033[0m'
+ERROR_MESSAGE="${ERROR_COLOR}ERROR:${NO_COLOR} Bootloader not found."
+
+
+# helper functions
+wait_for_bootloader() {
+	until dfu-util -l | grep -q "Found DFU"; do
+		echo "$ERROR_MESSAGE"
+		sleep 5
+	done
+}
+
+docker_make() {
+	docker run -u $USER --rm -it -v $QMK_SRC:$WORKDIR qmkfm/base_container \
+		make -C $WORKDIR $@
+}
+
+flash_image() {
+	dfu-util $DFU_ARGS -D $1
+}
+
+
+# begin script
+TARGET=${KEYBOARD}:${KEYMAP}
+IMAGE=${KEYBOARD}_${KEYMAP}.bin
+
+docker_make $TARGET
+wait_for_bootloader
+flash_image $IMAGE
+docker_make $TARGET MASTER=right
+wait_for_bootloader
+flash_image $IMAGE
